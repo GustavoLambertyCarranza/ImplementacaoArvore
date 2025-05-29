03880dcbc867e10c12ae3e478077095566723691
GustavoLambertyCarranza <168040500+GustavoLambertyCarranza@users.noreply.github.com>
2025-05-29 20:19:33 -0300

    Update No.java
    adicionado fator de balanceamento na classe no

--- Arvore/src/com/company/No.java
+++ Arvore/src/com/company/No.java
@@ -1,13 +1,23 @@
 package com.company;
 
 class No {
+    public boolean valor;
     String value;
-    No left, right;
+    No esquerda, direita;
+    int fatorBalanceamento;
 
     public No(String value) {
         this.value = value;
-        this.left = null;
-        this.right = null;
+        this.esquerda = null;
+        this.direita = null;
+        this.fatorBalanceamento = 0;
+    }
+
+    public int getFatorBalanceamento() {
+        return fatorBalanceamento;
+    }
+
+    public void setFatorBalanceamento(int fatorBalanceamento) {
+        this.fatorBalanceamento = fatorBalanceamento;
     }
 }
-
